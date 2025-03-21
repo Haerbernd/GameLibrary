b21e7637e287a9c66297baca569f4907f694fea0
Haerbernd <105395307+Haerbernd@users.noreply.github.com>
2025-03-21 17:15:01 +0100

    Initial Commit
    # Conflicts:
    #	info.yaml
    #	lib/customExceptions.py
    #	main.py

--- lib/customExceptions.py
+++ lib/customExceptions.py
@@ -1,13 +1,5 @@
-from lib.system_operations import OperatingSystem
-
 class UnsupportedArgumentException(Exception):
     def __init__(self, message: str = 'Unsupported Argument used...', code: int = 1, details=None):
         super().__init__(message)  # Pass the message to the base class
         self.code = code  # Custom attribute
         self.details = details
-
-class UnsupportedOperatingSystemException(Exception):
-    def __init__(self, os: OperatingSystem, message: str = ""):
-        if message == "":
-            message = f"Unsupported Operating System {os}..."
-        super().__init__(message)
--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-from lib import db_handler, logging_handler
-
-
 if __name__ == "__main__":
     pass
