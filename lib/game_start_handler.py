b2f2d0a2069c6fa59d8761a1a96759c8f7420b59
Haerbernd <105395307+Haerbernd@users.noreply.github.com>
2025-03-21 17:20:50 +0100

    Moved the `start_html` function to `game_start_handler` as it makes more sense to be there and is not implemented anywhere yet anyway.

--- lib/game_start_handler.py
+++ lib/game_start_handler.py
@@ -1,9 +1,5 @@
 import webbrowser
-from lib.game_handler import GameStartType
+
 
 def start_html(url: str) -> None:
     webbrowser.open_new(url)
-
-
-def start_game_directly(game_id: int, game_start_type: GameStartType):
-    pass
